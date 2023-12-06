9495d1f4d512758b188635b2c9ba3275651943c5
rtwohey <rtwohey@ionic.io>
2023-12-06 08:13:43 -0500

    added a broken and working pdf

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,40 @@
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import {getBrokenPdf} from './brokenpdf.js'
+import { getWorkingPdf } from './workingpdf.js';
 import './ExploreContainer.css';
+import { IonButton } from '@ionic/react';
+import { FileOpener } from '@capawesome-team/capacitor-file-opener';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+
+  const createPdf = async (broken: boolean) => {
+    const pdfData = broken ? getBrokenPdf() : getWorkingPdf();
+    console.log(pdfData)
+    const pdfFile = await Filesystem.writeFile({
+
+      path: `testpdf.pdf`,
+      
+      data: `data:application/pdf;base64,${pdfData}`,
+      
+      directory: Directory.ExternalStorage
+      
+      });
+
+      console.log(pdfFile)
+      const file = await Filesystem.readFile({path: pdfFile.uri})
+      console.log(file)
+
+      await FileOpener.openFile({path: pdfFile.uri});
+  }
+
+  
+
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton onClick={() => createPdf(true)}>Open Broken Pdf</IonButton>
+      <IonButton onClick={() => createPdf(false)}>Open Working Pdf</IonButton>
     </div>
   );
 };
